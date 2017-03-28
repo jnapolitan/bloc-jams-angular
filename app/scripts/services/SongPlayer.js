2ce79c764fa31803737cd32f26fa032258160063
Julian Napolitan <jnapolitan@twitter.com>
2017-03-28 16:50:22 -0700

    checkpoint 10 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
  (function () {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
          var SongPlayer = {};
 
          /**
@@ -26,7 +26,19 @@
                  preload: true
              });
 
+             currentBuzzObject.bind('timeupdate', function () {
+                 $rootScope.$apply(function () {
+                     SongPlayer.currentTime = currentBuzzObject.getTime();
+                 });
+             });
+
              SongPlayer.currentSong = song;
+
+             /**
+              * @desc Current playback time (in seconds) of currently playing song
+              * @type {Number}
+              */
+             SongPlayer.currentTime = null;
          };
 
          /**
@@ -144,10 +156,21 @@
              }
          };
 
+         /**
+          * @function setCurrentTime
+          * @desc Set current time (in seconds) of currently playing song
+          * @param {Number} time
+          */
+         SongPlayer.setCurrentTime = function (time) {
+             if (currentBuzzObject) {
+                 currentBuzzObject.setTime(time);
+             }
+         };
+
          return SongPlayer;
      }
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
